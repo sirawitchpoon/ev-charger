2ac7fb4a3a0008f3d49645314662f61d8b237ac9
Sirawitch Butryojantho <poonnyworld@gmail.com>
2026-04-23 02:02:36 +0700

    Initial Commit

---
+++ ev-charger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ev-charger
+//
+//  Created by Sirawitch Butryojantho on 23/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ev-charger/ev_chargerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ev_chargerApp.swift
+//  ev-charger
+//
+//  Created by Sirawitch Butryojantho on 23/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct ev_chargerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
